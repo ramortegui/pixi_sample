1d9784cd7620270e6e1a0e43c70346ddd01a434c
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-11-25 08:33:21 -0700

    Add a rotating bunny.

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,52 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import * as PIXI from "pixi.js";
+
+
+
+
+let type = "WebGL"
+if(!PIXI.utils.isWebGLSupported()){
+  type = "canvas"
+}
+PIXI.utils.sayHello(type)
+
+//Create a Pixi Application
+let app = new PIXI.Application({
+    width: 256,         // default: 800
+    height: 256,        // default: 600
+    antialias: true,    // default: false
+    transparent: false, // default: false
+    resolution: 1       // default: 1
+  }
+);
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+
+// load the texture we need
+PIXI.loader.add('bunny', 'images/bunny.png').load((loader, resources) => {
+    // This creates a texture from a 'bunny.png' image
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+
+    // Rotate around the center
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // Add the bunny to the scene we are building
+    app.stage.addChild(bunny);
+
+    // Listen for frame updates
+    app.ticker.add(() => {
+         // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+    });
+});
+
---
+++ assets/js/load_pixi.js
@@ -0,0 +1,7 @@
+
+
+  let type = "WebGL"
+  if(!PIXI.utils.isWebGLSupported()){
+    type = "canvas"
+  }
+  PIXI.utils.sayHello(type)
