23200230b41698cad25e77a9ae10e2aa6fe034ac
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-11-29 23:25:27 -0700

    Add a registry for processes names.

--- lib/pixi_sample/application.ex
+++ lib/pixi_sample/application.ex
@@ -9,7 +9,8 @@ defmodule PixiSample.Application do
     # List all child processes to be supervised
     children = [
       # Start the endpoint when the application starts
-      PixiSampleWeb.Endpoint
+      PixiSampleWeb.Endpoint,
+      PixiSample.Sprite.Registry
       # Starts a worker by calling: PixiSample.Worker.start_link(arg)
       # {PixiSample.Worker, arg},
     ]
